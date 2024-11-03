6d7bd678de2abb4403aa03a3bb05104c83409534
Jason Hoang <jvhoang@andrew.cmu.edu>
2024-11-03 15:38:18 -0500

    mt try 2

--- skateback-pi/motors/jason.py
+++ skateback-pi/motors/jason.py
@@ -2,7 +2,8 @@ import pyvesc
 from pyvesc.VESC.messages import SetDutyCycle
 import serial
 import time
-import curses
+from pynput import keyboard
+import threading
 
 # Initialize duty cycles and acceleration steps
 left_duty_cycle = 0.0
@@ -15,55 +16,66 @@ braking_deceleration_step = 0.04  # Larger step for faster deceleration (braking
 left_serial_port = '/dev/ttyACM0'
 right_serial_port = '/dev/ttyACM1'
 
+# Initialize key state
+pressed_keys = set()
+keys_lock = threading.Lock()
+
 def set_motor_duty_cycle(serial_port, duty_cycle):
     """Set the motor to the given duty cycle."""
     with serial.Serial(serial_port, baudrate=115200, timeout=0.05) as ser:
         ser.write(pyvesc.encode(SetDutyCycle(duty_cycle)))
 
-def control_motors(stdscr):
-    global left_duty_cycle, right_duty_cycle
-    stdscr.nodelay(True)  # Non-blocking key input
+def on_press(key):
+    with keys_lock:
+        pressed_keys.add(key)
 
-    # Flags to track if keys are being held down
-    left_motor_active = False
-    right_motor_active = False
+def on_release(key):
+    with keys_lock:
+        pressed_keys.discard(key)
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+def control_motors():
+    global left_duty_cycle, right_duty_cycle
     left_braking = False
     right_braking = False
 
     try:
         while True:
-            key = stdscr.getch()
+            with keys_lock:
+                keys = pressed_keys.copy()
 
             # Control Left Motor
-            if key == ord('w'):  # Accelerate left motor
+            if keyboard.KeyCode.from_char('w') in keys:  # Accelerate left motor
                 left_duty_cycle += acceleration_step
                 if left_duty_cycle > 1:
                     left_duty_cycle = 1
                 left_motor_active = True
                 left_braking = False
-                stdscr.addstr(0, 0, f"Left motor accelerating: {left_duty_cycle}")
+                print(f"Left motor accelerating: {left_duty_cycle}")
 
-            elif key == ord('s'):  # Decelerate left motor faster (braking)
+            elif keyboard.KeyCode.from_char('s') in keys:  # Decelerate left motor faster (braking)
                 left_braking = True
                 left_motor_active = False
-                stdscr.addstr(1, 0, f"Left motor braking: {left_duty_cycle}")
+                print(f"Left motor braking: {left_duty_cycle}")
 
             else:
                 left_motor_active = False
 
             # Control Right Motor
-            if key == curses.KEY_UP:  # Accelerate right motor
+            if keyboard.Key.up in keys:  # Accelerate right motor
                 right_duty_cycle += acceleration_step
                 if right_duty_cycle > 1:
                     right_duty_cycle = 1
                 right_motor_active = True
                 right_braking = False
-                stdscr.addstr(2, 0, f"Right motor accelerating: {right_duty_cycle}")
+                print(f"Right motor accelerating: {right_duty_cycle}")
 
-            elif key == curses.KEY_DOWN:  # Decelerate right motor faster (braking)
+            elif keyboard.Key.down in keys:  # Decelerate right motor faster (braking)
                 right_braking = True
                 right_motor_active = False
-                stdscr.addstr(3, 0, f"Right motor braking: {right_duty_cycle}")
+                print(f"Right motor braking: {right_duty_cycle}")
 
             else:
                 right_motor_active = False
@@ -124,16 +136,19 @@ def control_motors(stdscr):
             set_motor_duty_cycle(left_serial_port, left_duty_cycle)
             set_motor_duty_cycle(right_serial_port, right_duty_cycle)
 
-            stdscr.refresh()
             time.sleep(0.1)
 
     except KeyboardInterrupt:
         # Stop motors if the script is interrupted
         set_motor_duty_cycle(left_serial_port, 0)
         set_motor_duty_cycle(right_serial_port, 0)
-        stdscr.addstr(4, 0, "Motors stopped")
-        stdscr.refresh()
+        print("Motors stopped")
         time.sleep(1)
 
 if __name__ == "__main__":
-    curses.wrapper(control_motors)
+    listener = keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release)
+    listener.start()
+
+    control_motors()
