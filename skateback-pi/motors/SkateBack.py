c1ad41a143ac040b8ffd2d8a6907df3a9d10d455
tajani-hub <tioluwajani52@gmail.com>
2024-11-03 15:08:44 -0500

    feat: wrote initial SkateBack class for motor commands

--- skateback-pi/motors/SkateBack.py
+++ skateback-pi/motors/SkateBack.py
@@ -1,7 +1,7 @@
-import pyvesc
-from pyvesc.VESC.messages import SetDutyCycle
 import serial
 import time
+import pyvesc
+from pyvesc import SetDutyCycle, SetCurrent, GetValues
 
 ACC_STEP = 0.01    # Duty Cycle step for acceleration
 DEC_STEP = 0.02    # Duty Cycle step for deceleration
@@ -9,7 +9,6 @@ BRK_STEP = 0.04    # Duty Cycle step for breaking
 SERIAL_L = '/dev/ttyACM0'    # Serial Port for left motor
 SERIAL_R = '/dev/ttyACM1'    # Serial Port for right motor
 MAX_DUTY_CYCLE = 1.0    # Max duty cycle
-
 
 class SkateBack:
     def __init__(self):
@@ -21,8 +20,9 @@ class SkateBack:
         
     """
     Create a timer that returns true over a given duration
+    [in] duration: amount of time to return true
     """
-    def create_timer(duration):
+    def create_timer(self, duration):
         start_time = time.time()
         def time_check():
             return (time.time() - start_time) < duration
@@ -34,17 +34,17 @@ class SkateBack:
     [in] duration: amount of time to maintain duty cycle
     [in] dut_cycle: duty cycle to set wheel at, magnitude cannot exceed MAX_DUTY_CYCLE
     """
-    def spin_motor(wheel, duration, duty_cycle):  
+    def set_duty_cycle(self, wheel, duration, duty_cycle):  
         # Set serialPort according to wheel parameter
         if wheel == "L":
             serialPort = SERIAL_L
         elif wheel == "R":
             serialPort = SERIAL_R
         else:
-            error("Please specify L or R for wheel")
+            raise Exception("Please specify L or R for wheel")
             
         # Create timer
-        timer = create_timer(duration)
+        timer = self.create_timer(duration)
         
         with serial.Serial(serialPort, baudrate=115200, timeout=0.05) as ser:
             try:
@@ -62,120 +62,71 @@ class SkateBack:
                 # Close serial connection
                 ser.close()
 
-def control_motors(stdscr):
-    global left_duty_cycle, right_duty_cycle
-    stdscr.nodelay(True)  # Non-blocking key input
+    """
+    Get the current duty cycle for a motor
+    [in] wheel: 'L' for left, 'R' for right, else throw error
+    [out] duty_cycle: Current duty cycle for specified wheel
+    """
+    def get_duty_cycle(self, wheel):
+        # Set serialPort according to wheel parameter
+        if wheel == "L":
+            serialPort = SERIAL_L
+        elif wheel == "R":
+            serialPort = SERIAL_R
+        else:
+            raise Exception("Please specify L or R for wheel")
+        
+        with serial.Serial(serialPort, baudrate=115200, timeout=0.05) as ser:
+            while True:
+                ser.write(pyvesc.encode_request(GetValues))
+                if ser.in_waiting > 61:
+                    (response, consumed) = pyvesc.decode(ser.read(61))
 
-    # Flags to track if keys are being held down
-    left_motor_active = False
-    right_motor_active = False
-    left_braking = False
-    right_braking = False
+                    try:
+                        return(response.duty_cycle_now)
+                    except:
+                        print("An error occured while waiting for a response")
+                        pass
 
-    try:
-        while True:
-            key = stdscr.getch()
+                # is this line necessary? it could stall what ever function calls set_duty_cycle
+                time.sleep(0.1)
 
-            # Control Left Motor
-            if key == ord('w'):  # Accelerate left motor
-                left_duty_cycle += acceleration_step
-                if left_duty_cycle > 1:
-                    left_duty_cycle = 1
-                left_motor_active = True
-                left_braking = False
-                stdscr.addstr(0, 0, f"Left motor accelerating: {left_duty_cycle}")
+    """
+    Accelerate a wheel using gradual increments of duty cycle
+    [in] wheel: 'L' for left, 'R' for right, else throw error
+    [in] target_duty_cycle: increment duty cycle until we reach this value
+    """
+    def accelerate_to(self, wheel, target_duty_cycle):
+        current_duty_cycle = self.get_duty_cycle(wheel)
 
-            elif key == ord('s'):  # Decelerate left motor faster (braking)
-                left_braking = True
-                left_motor_active = False
-                stdscr.addstr(1, 0, f"Left motor braking: {left_duty_cycle}")
+        while current_duty_cycle < target_duty_cycle <= MAX_DUTY_CYCLE:
+            self.set_duty_cycle(wheel, duration=0.1, duty_cycle=current_duty_cycle)
+            current_duty_cycle += ACC_STEP
 
-            else:
-                left_motor_active = False
+    """
+    Decelerate a wheel using gradual decrements of duty cycle
+    [in] wheel: 'L' for left, 'R' for right, else throw error
+    [in] target_duty_cycle: decrement duty cycle until we reach this value
+    """
+    def decelerate_to(self, wheel, target_duty_cycle):
+        current_duty_cycle = self.get_duty_cycle(wheel)
 
-            # Control Right Motor
-            if key == curses.KEY_UP:  # Accelerate right motor
-                right_duty_cycle += acceleration_step
-                if right_duty_cycle > 1:
-                    right_duty_cycle = 1
-                right_motor_active = True
-                right_braking = False
-                stdscr.addstr(2, 0, f"Right motor accelerating: {right_duty_cycle}")
+        while current_duty_cycle > target_duty_cycle >= 0.0:
+            self.set_duty_cycle(wheel, duration=0.1, duty_cycle=current_duty_cycle)
+            current_duty_cycle -= DEC_STEP 
 
-            elif key == curses.KEY_DOWN:  # Decelerate right motor faster (braking)
-                right_braking = True
-                right_motor_active = False
-                stdscr.addstr(3, 0, f"Right motor braking: {right_duty_cycle}")
+    """
+    Bring duty cycle of a wheel to zero
+    [in] wheel: 'L' for left, 'R' for right, else throw error
+    """   
+    def brake(self, wheel):
+        current_duty_cycle = self.get_duty_cycle(wheel)
 
-            else:
-                right_motor_active = False
-
-            # Deceleration for Left Motor
-            if not left_motor_active:
-                if left_braking:
-                    # Apply faster deceleration for braking
-                    if left_duty_cycle > 0:
-                        left_duty_cycle -= braking_deceleration_step
-                        if left_duty_cycle < 0:
-                            left_duty_cycle = 0
-                    elif left_duty_cycle < 0:
-                        left_duty_cycle += braking_deceleration_step
-                        if left_duty_cycle > 0:
-                            left_duty_cycle = 0
-                else:
-                    # Apply normal smooth deceleration
-                    if left_duty_cycle > 0:
-                        left_duty_cycle -= normal_deceleration_step
-                        if left_duty_cycle < 0:
-                            left_duty_cycle = 0
-                    elif left_duty_cycle < 0:
-                        left_duty_cycle += normal_deceleration_step
-                        if left_duty_cycle > 0:
-                            left_duty_cycle = 0
-
-            # Deceleration for Right Motor
-            if not right_motor_active:
-                if right_braking:
-                    # Apply faster deceleration for braking
-                    if right_duty_cycle > 0:
-                        right_duty_cycle -= braking_deceleration_step
-                        if right_duty_cycle < 0:
-                            right_duty_cycle = 0
-                    elif right_duty_cycle < 0:
-                        right_duty_cycle += braking_deceleration_step
-                        if right_duty_cycle > 0:
-                            right_duty_cycle = 0
-                else:
-                    # Apply normal smooth deceleration
-                    if right_duty_cycle > 0:
-                        right_duty_cycle -= normal_deceleration_step
-                        if right_duty_cycle < 0:
-                            right_duty_cycle = 0
-                    elif right_duty_cycle < 0:
-                        right_duty_cycle += normal_deceleration_step
-                        if right_duty_cycle > 0:
-                            right_duty_cycle = 0
-
-            # Reset braking flags if duty cycle reaches zero
-            if left_duty_cycle == 0:
-                left_braking = False
-            if right_duty_cycle == 0:
-                right_braking = False
-
-            # Continuously send the current duty cycle for both motors
-            set_motor_duty_cycle(left_serial_port, left_duty_cycle)
-            set_motor_duty_cycle(right_serial_port, right_duty_cycle)
-
-            stdscr.refresh()
-            time.sleep(0.1)
-
-    except KeyboardInterrupt:
-        # Stop motors if the script is interrupted
-        set_motor_duty_cycle(left_serial_port, 0)
-        set_motor_duty_cycle(right_serial_port, 0)
-        stdscr.addstr(4, 0, "Motors stopped")
-        stdscr.refresh()
-        time.sleep(1)
+        while current_duty_cycle > 0.0:
+            self.set_duty_cycle(wheel, duration=0.1, duty_cycle=current_duty_cycle)
+            current_duty_cycle -= BRK_STEP
 
 if __name__ == "__main__":
-    curses.wrapper(control_motors)
+    sk = SkateBack()
+    sk.accelerate_to("L", 0.5)
+    sk.accelerate_to("R", 0.5)
